b43f75825f99e10cfa29d21b36defdee966193ac
shubham <shagrawal@grepruby.io>
2019-02-27 17:34:04 +0530

    add attribute content to comments

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_27_114631) do
+ActiveRecord::Schema.define(version: 2019_02_27_120256) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2019_02_27_114631) do
   create_table "comments", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "content"
   end
 
   create_table "posts", force: :cascade do |t|
