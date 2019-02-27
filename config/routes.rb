d69f09e501e9c601dad4647ce5c93746ca2cad40
shubham <shagrawal@grepruby.io>
2019-02-27 13:28:32 +0530

    set routes for root and posts resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get 'home/index'
+
+  resources :posts
+
   devise_for :users
+  root 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
