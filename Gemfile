0c1f9bfae4e41c5fc97bc606f248b7e7cf2e259e
shubham <shagrawal@grepruby.io>
2019-03-07 13:39:52 +0530

    added jquery support and gems

--- Gemfile
+++ Gemfile
@@ -3,6 +3,10 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby '2.6.0'
 
+gem 'jquery-rails'
+
+gem 'font-awesome-rails'
+gem 'bootstrap', '~> 4.3.1'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.2'
 # Use postgresql as the database for Active Record
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,3 +1,5 @@
+
+
 // This is a manifest file that'll be compiled into application.js, which will include all the files
 // listed below.
 //
@@ -10,7 +12,8 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require rails-ujs
+//= require jquery3
+//= require jquery_ujs
 //= require activestorage
 //= require turbolinks
 //= require_tree .
