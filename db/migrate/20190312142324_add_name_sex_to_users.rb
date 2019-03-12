39d86c77319c33d6129a8d6429e4d1e0f2314c34
shubham <shagrawal@grepruby.io>
2019-03-13 00:47:28 +0530

    combined two migrations

--- db/migrate/20190312142324_add_name_sex_to_users.rb
+++ db/migrate/20190312142324_add_name_sex_to_users.rb
@@ -1,6 +1,7 @@
-class AddNameToUsers < ActiveRecord::Migration[5.2]
+class AddNameSexToUsers < ActiveRecord::Migration[5.2]
   def change
     add_column :users, :first_name, :string
     add_column :users, :last_name, :string
+    add_column :users, :sex, :string
   end
 end
