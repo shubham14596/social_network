0b3baebdf6ec4b8b7926b6bc758ff7c6b8d3edf8
shubham <shagrawal@grepruby.io>
2019-03-12 15:03:39 +0530

    moved query to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,10 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :posts
   has_many :comments
+  has_many :likes
+
+  def liked?(likable)
+  	self.likes.find_by(likable_id: likable.id, likable_type: likable.class.name)
+  end
+
 end
