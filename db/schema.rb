b33dba61d8138afdbf566b38c34135c3748d9690
shubham <shagrawal@grepruby.io>
2019-03-12 14:43:06 +0530

    tables migrated

--- db/schema.rb
+++ db/schema.rb
@@ -16,11 +16,13 @@ ActiveRecord::Schema.define(version: 2019_03_07_132620) do
   enable_extension "plpgsql"
 
   create_table "comments", force: :cascade do |t|
+    t.string "content"
+    t.bigint "post_id"
+    t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "content"
-    t.integer "post_id"
-    t.integer "user_id"
+    t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "likes", force: :cascade do |t|
@@ -53,5 +55,7 @@ ActiveRecord::Schema.define(version: 2019_03_07_132620) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "comments", "posts"
+  add_foreign_key "comments", "users"
   add_foreign_key "likes", "users"
 end
